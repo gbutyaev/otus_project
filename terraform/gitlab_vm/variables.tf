d07147bad29c75e7e173686ea97a01e529d059a8
g.butyaev <g.butyaev@rt-solar.ru>
2022-05-06 05:08:22 +0300

    added infra

---
+++ terraform/gitlab_vm/variables.tf
@@ -0,0 +1,27 @@
+variable cloud_id {
+  description = "Cloud"
+}
+variable folder_id {
+  description = "Folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "public_key"
+}
+variable image_id {
+  description = "Disk image"
+}
+variable subnet_id {
+  description = "Subnet"
+}
+variable token {
+  description = "token"
+}
+variable private_key_path {
+  description = "private_key"
+}
